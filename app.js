c0e909f0621c846342cdd320811a94dbe05a760d
maria dhaybi <maria@gmail.com>
2024-12-04 17:12:47 +0200

    adding new endpoint

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ const PORT = process.env.PORT || 3030;
 app.get('/', function (req, res) {
     res.send('Hello World')
   })
+  app.get('/students', function (req, res) {
+    var data =[
+      {id:1,name:'maria dh'},
+      {id:1,name:'rouaa'},
+      {id:1,name:'serena'}
+    ]
+    res.send(data)
+  })
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
