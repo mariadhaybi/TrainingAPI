7cff76f70f4e903cd6a630ec2cb38bb4ce351d0e
maria dhaybi <maria@gmail.com>
2024-12-05 12:43:38 +0200

    c5

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
 const express = require("express");
 const app = express();
 //require('dotenv').config()
+const xlsx = require('xlsx');
+let wb = xlsx.readFile('data.xlsx');
+let ws=wb.Sheets['names'];
+let _data = xlsx.utils.sheet_to_json(ws);
 const PORT = process.env.PORT || 3030;
 //console.log(process.env.MARIA)
 
-// your code
+
 app.get('/', function (req, res) {
     res.send('Hello World')
   })
   app.get('/students', function (req, res) {
-    var data =[
-      {id:1,name:'maria dh'},
-      {id:1,name:'rouaa'},
-      {id:1,name:'serena'}
-    ]
-    res.send(data)
+    // var data =[
+    //   {id:1,name:'maria dh'},
+    //   {id:1,name:'rouaa'},
+    //   {id:1,name:'serena'}
+    // ]
+    res.send(_data )
   })
 
 app.listen(PORT, () => {
